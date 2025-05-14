cb6ac6cdf62f288531bf7370ffeb69b085ac1bef
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-05-14 08:12:24 +0200

    add Razor class library (*.Razor.Employees)

---
+++ Northwind.Razor.Employees/Areas/PacktFeatures/Pages/Employees.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;  // PageModel
+using Packt.Shared;  // NorthwindContext, Employee
+
+namespace PacktFeatures.Pages;
+
+public class EmployeesPageModel : PageModel
+{
+    private NorthwindContext _db;
+
+    public EmployeesPageModel(NorthwindContext injectedContext)
+    {
+        _db = injectedContext;
+    }
+
+    public Employee[] Employees { get; set; } = null!;
+
+    public void OnGet()
+    {
+        ViewData["Title"] = "Northwind B2B - Employees";
+        Employees = _db.Employees
+            .OrderBy(e => e.LastName)
+            .ThenBy(e => e.FirstName)
+            .ToArray();
+    }
+}
